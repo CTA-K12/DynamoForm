f187df22beef34e8f8f5089cd5953484469ff5a5
dcramble <dcramble@mesd.k12.or.us>
2016-05-25 11:22:43 -0700

    Clean-ups

--- vendor/selectize/js/selectize.js
+++ vendor/selectize/js/selectize.js
@@ -3644,8 +3644,6 @@
 	});
 
 	Selectize.define('remove_button', function(options) {
-		if (this.settings.mode === 'single') return;
-
 		options = $.extend({
 			label     : '&times;',
 			title     : 'Remove',
@@ -3653,50 +3651,106 @@
 			append    : true
 		}, options);
 
-		var self = this;
-		var html = '<a href="javascript:void(0)" class="' + options.className + '" tabindex="-1" title="' + escape_html(options.title) + '">' + options.label + '</a>';
+		var singleClose = function(thisRef, options) {
 
-		/**
-		 * Appends an element as a child (with raw HTML).
-		 *
-		 * @param {string} html_container
-		 * @param {string} html_element
-		 * @return {string}
-		 */
-		var append = function(html_container, html_element) {
-			var pos = html_container.search(/(<\/[^>]+>\s*)$/);
-			return html_container.substring(0, pos) + html_element + html_container.substring(pos);
+			options.className = 'remove-single';
+
+			var self = thisRef;
+			var html = '<a href="javascript:void(0)" class="' + options.className + '" tabindex="-1" title="' + escape_html(options.title) + '">' + options.label + '</a>';
+
+			/**
+			 * Appends an element as a child (with raw HTML).
+			 *
+			 * @param {string} html_container
+			 * @param {string} html_element
+			 * @return {string}
+			 */
+			var append = function(html_container, html_element) {
+				return html_container + html_element;
+			};
+
+			thisRef.setup = (function() {
+				var original = self.setup;
+				return function() {
+					// override the item rendering method to add the button to each
+					if (options.append) {
+						var id = $(self.$input.context).attr('id');
+						var selectizer = $('#'+id);
+
+						var render_item = self.settings.render.item;
+						self.settings.render.item = function(data) {
+							return append(render_item.apply(thisRef, arguments), html);
+						};
+					}
+
+					original.apply(thisRef, arguments);
+
+					// add event listener
+					thisRef.$control.on('click', '.' + options.className, function(e) {
+						e.preventDefault();
+						if (self.isLocked) return;
+
+						self.clear();
+					});
+
+				};
+			})();
 		};
 
-		this.setup = (function() {
-			var original = self.setup;
-			return function() {
-				// override the item rendering method to add the button to each
-				if (options.append) {
-					var render_item = self.settings.render.item;
-					self.settings.render.item = function(data) {
-						return append(render_item.apply(this, arguments), html);
-					};
-				}
+		var multiClose = function(thisRef, options) {
 
-				original.apply(this, arguments);
+			var self = thisRef;
+			var html = '<a href="javascript:void(0)" class="' + options.className + '" tabindex="-1" title="' + escape_html(options.title) + '">' + options.label + '</a>';
 
-				// add event listener
-				this.$control.on('click', '.' + options.className, function(e) {
-					e.preventDefault();
-					if (self.isLocked) return;
-
-					var $item = $(e.currentTarget).parent();
-					self.setActiveItem($item);
-					if (self.deleteSelection()) {
-						self.setCaret(self.items.length);
-					}
-				});
-
+			/**
+			 * Appends an element as a child (with raw HTML).
+			 *
+			 * @param {string} html_container
+			 * @param {string} html_element
+			 * @return {string}
+			 */
+			var append = function(html_container, html_element) {
+				var pos = html_container.search(/(<\/[^>]+>\s*)$/);
+				return html_container.substring(0, pos) + html_element + html_container.substring(pos);
 			};
-		})();
 
+			thisRef.setup = (function() {
+				var original = self.setup;
+				return function() {
+					// override the item rendering method to add the button to each
+					if (options.append) {
+						var render_item = self.settings.render.item;
+						self.settings.render.item = function(data) {
+							return append(render_item.apply(thisRef, arguments), html);
+						};
+					}
+
+					original.apply(thisRef, arguments);
+
+					// add event listener
+					thisRef.$control.on('click', '.' + options.className, function(e) {
+						e.preventDefault();
+						if (self.isLocked) return;
+
+						var $item = $(e.currentTarget).parent();
+						self.setActiveItem($item);
+						if (self.deleteSelection()) {
+							self.setCaret(self.items.length);
+						}
+					});
+
+				};
+			})();
+		};
+
+		if (this.settings.mode === 'single') {
+			singleClose(this, options);
+			return;
+		} else {
+			multiClose(this, options);
+		}
 	});
+
 
 	Selectize.define('restore_on_backspace', function(options) {
 		var self = this;
@@ -3759,7 +3813,7 @@
 	            });
 
 	            this.$control_input.on('blur', function() {
-	                self.setValue(self.typingValue);
+	                self.setValue(self.typingValue, true);
 	            });
 	        };
 	    })();
